259362791866659985d8747e4ed05101b6b28f8c
Niyas Ali M K <niyasalimk132@gmail.com>
2025-06-29 09:14:03 +0530

    03 home layout

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,8 +2,12 @@ import { Button } from "@/components/ui/button";
 import { Checkbox } from "@/components/ui/checkbox";
 import { Progress } from "@/components/ui/progress";
 import { Textarea } from "@/components/ui/textarea";
+import { Input } from "@/components/ui/input";
+import { useEffect } from "react";
 
 export default function Home() {
+
+
   return (
     <div className="p-4">
     <div className="flex flex-col gap-y-4">
@@ -13,7 +17,7 @@ export default function Home() {
         </Button>
       </div>
       <div>
-        <input placeholder="IAm a Input"/>
+        <Input placeholder="IAm a Input"></Input>
       </div>
       <div>
         <Progress value={50}></Progress>
